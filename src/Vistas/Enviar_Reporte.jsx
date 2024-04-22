8d9e61531665abdb16db123260c217d8f85374f0
Reyes-Maicol <reyesmaicol225@gmail.com>
2024-04-22 16:18:03 -0500

    Se hace la estructura de las tablas para mostrar la informacion del alumno y todo acerca del mismo

--- src/Vistas/Enviar_Reporte.jsx
+++ src/Vistas/Enviar_Reporte.jsx
@@ -1,31 +1,21 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-=======
-import React, { useState, useEffect, useRef} from "react";
->>>>>>> 421a59555236f64615481aaac88a65c1afa7454f
+import React, { useState, useRef } from "react";
 import axios from 'axios';
 import SidebarAdmi from "../Componentes/Dashboard_admi";
 import Buscar from "../Imagenes/iconos/bx-search.svg";
 import '../css/Reportes.css'
 
 const Enviar_Reporte = () => {
-<<<<<<< HEAD
-    const [datosEstudiante, setDatosEstudiante] = useState(null);
-    const [numero_id, setNumero_id] = useState('');
-    const [mostrarTablas, setMostrarTablas] = useState(false);
-    
-=======
 
+    //Mostrar y ocultar el sidebar
     var refmove = useRef();
     const [showe, setShowe] = useState(false);
     const move_conte = (e) => {
         setShowe(!showe)
     }
+    const [datosEstudiante, setDatosEstudiante] = useState(null);
+    const [numero_id, setNumero_id] = useState('');
+    const [mostrarTablas, setMostrarTablas] = useState(false);
 
-    const [reportes, setReportes] = useState([]);
-    const [numero_id, setnumero_id] = useState('');
-
->>>>>>> 421a59555236f64615481aaac88a65c1afa7454f
     const handleSubmit = async (e) => {
         e.preventDefault();
 
@@ -54,95 +44,96 @@ const Enviar_Reporte = () => {
     const handleInputChange = (e) => {
         setNumero_id(e.target.value);
     };
-
     return (
-        <div className={`${showe ? 'space-toggle': null}`} ref={refmove}>
-            <SidebarAdmi Move={move_conte}/>
-            <div className="info-text">
-                <h1>Reporte Alumno</h1>
-                <form onSubmit={handleSubmit}>
-                    <div className="buscar">
-                        <input
-                            type="text"
-                            placeholder="Ingrese número de documento del estudiante"
-                            value={numero_id}
-                            onChange={handleInputChange}
-                            required
-                        />
-                        <button type="submit">
-                            <img src={Buscar} alt="Buscar" />
-                        </button>
-                    </div>
-                </form>
-                {mostrarTablas && datosEstudiante && (
-                    <>
-                        <h2>Datos del Estudiante</h2>
-                        <table className="table_reporte" border="1">
-                            <thead>
-                                <tr>
-                                    <th>Nombre Alumno</th>
-                                    <th>Apellido Alumno</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>{datosEstudiante.Nombre}</td>
-                                    <td>{datosEstudiante.Apellido}</td>
-                                </tr>
-                            </tbody>
-                        </table>
+        <div className={`contenert ${showe ? 'space-toggle' : null}`} ref={refmove}>
+            <div className="contenert">
+                <SidebarAdmi Move={move_conte} />
+                <div className="info-text">
+                    <h1>Reporte Alumno</h1>
+                    <form onSubmit={handleSubmit}>
+                        <div className="buscar">
+                            <input
+                                type="text"
+                                placeholder="Ingrese número de documento del estudiante"
+                                value={numero_id}
+                                onChange={handleInputChange}
+                                required
+                            />
+                            <button type="submit">
+                                <img src={Buscar} alt="Buscar" />
+                            </button>
+                        </div>
+                    </form>
+                    {mostrarTablas && datosEstudiante && (
+                        <>
+                            <h2>Datos del Estudiante</h2>
+                            <table className="table_reporte" border="1">
+                                <thead>
+                                    <tr>
+                                        <th>Nombre Alumno</th>
+                                        <th>Apellido Alumno</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>{datosEstudiante.Nombre}</td>
+                                        <td>{datosEstudiante.Apellido}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
 
-                        <h2>Actividades Inscritas</h2>
-                        <table className="table_reporte" border="1">
-                            <thead>
-                                <tr>
-                                    <th>Actividades Inscritas</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {datosEstudiante.resultadoDatosEstudiante &&
-                                    Array.isArray(datosEstudiante.resultadoDatosEstudiante) &&
-                                    Array.from(
-                                        new Set(
-                                            datosEstudiante.resultadoDatosEstudiante.flatMap((reporte) =>
-                                                reporte.Actividades_Inscritas.split(', ')
+                            <h2>Actividades Inscritas</h2>
+                            <table className="table_reporte" border="1">
+                                <thead>
+                                    <tr>
+                                        <th>Actividades Inscritas</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {datosEstudiante.resultadoDatosEstudiante &&
+                                        Array.isArray(datosEstudiante.resultadoDatosEstudiante) &&
+                                        Array.from(
+                                            new Set(
+                                                datosEstudiante.resultadoDatosEstudiante.flatMap((reporte) =>
+                                                    reporte.Actividades_Inscritas.split(', ')
+                                                )
                                             )
-                                        )
-                                    ).map((actividad) => (
-                                        <tr key={actividad}>
-                                            <td>{actividad}</td>
-                                        </tr>
-                                    ))}
-                            </tbody>
-                        </table>
+                                        ).map((actividad) => (
+                                            <tr key={actividad}>
+                                                <td>{actividad}</td>
+                                            </tr>
+                                        ))}
+                                </tbody>
+                            </table>
 
-                        <h2>Detalles del Estudiante</h2>
-                        <table className="table_reporte" border="1">
-                            <thead>
-                                <tr>
-                                    <th>Fecha Observación</th>
-                                    <th>Descripción Observación</th>
-                                    <th>Nombre Actividad</th>
-                                    <th>Total Inasistencias</th>
-                                    <th>Total Puntos</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {datosEstudiante.resultadoDatosEstudiante &&
-                                    Array.isArray(datosEstudiante.resultadoDatosEstudiante) &&
-                                    datosEstudiante.resultadoDatosEstudiante.map((reporte) => (
-                                        <tr key={`${reporte.Nombre_actividad}-${reporte.fecha_observacion}`}>
-                                            <td>{reporte.fecha_observacion || 'N/A'}</td>
-                                            <td>{reporte.descripcion_observacion || 'N/A'}</td>
-                                            <td>{reporte.Nombre_actividad || 'N/A'}</td>
-                                            <td>{reporte.Inasistencias ?? 'N/A'}</td>
-                                            <td>{reporte.Puntos_Totales || 'N/A'}</td>
-                                        </tr>
-                                    ))}
-                            </tbody>
-                        </table>
-                    </>
-                )}
+                            <h2>Detalles del Estudiante</h2>
+                            <table className="table_reporte" border="1">
+                                <thead>
+                                    <tr>
+                                        <th>Fecha Observación</th>
+                                        <th>Descripción Observación</th>
+                                        <th>Nombre Actividad</th>
+                                        <th>Total Inasistencias</th>
+                                        <th>Total Puntos</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {datosEstudiante.resultadoDatosEstudiante &&
+                                        Array.isArray(datosEstudiante.resultadoDatosEstudiante) &&
+                                        datosEstudiante.resultadoDatosEstudiante.map((reporte) => (
+                                            <tr key={`${reporte.Nombre_actividad}-${reporte.fecha_observacion}`}>
+                                                <td>{reporte.fecha_observacion || 'N/A'}</td>
+                                                <td>{reporte.descripcion_observacion || 'N/A'}</td>
+                                                <td>{reporte.Nombre_actividad || 'N/A'}</td>
+                                                <td>{reporte.Inasistencias ?? 'N/A'}</td>
+                                                <td>{reporte.Puntos_Totales || 'N/A'}</td>
+                                            </tr>
+                                        ))}
+                                </tbody>
+                            </table>
+                        </>
+                    )}
+                </div>
             </div>
         </div>
     );
